03023217cf903b3370efeae45de24563c11a90c7
Kidist <99483726+15997@users.noreply.github.com>
2026-04-19 23:50:47 +0300

    app_theme.dart

--- Front-end/lib/core/app_theme.dart
+++ Front-end/lib/core/app_theme.dart
@@ -1,1 +1,29 @@
+import 'package:flutter/material.dart';
 
+class AppTheme {
+  static const Color primaryTeal = Color(0xFF008080);
+  static const Color accentOrange = Colors.orange;
+  static const Color bgLightGrey = Color(0xFFF3F4F6);
+
+  static ThemeData get lightTheme {
+    return ThemeData(
+      primaryColor: primaryTeal,
+      fontFamily: 'Roboto',
+      scaffoldBackgroundColor: Colors.white,
+      appBarTheme: const AppBarTheme(
+        backgroundColor: Colors.white,
+        elevation: 0,
+        iconTheme: IconThemeData(color: Colors.black),
+        titleTextStyle: TextStyle(color: Colors.black, fontSize: 18, fontWeight: FontWeight.bold),
+      ),
+      elevatedButtonTheme: ElevatedButtonThemeData(
+        style: ElevatedButton.styleFrom(
+          backgroundColor: primaryTeal,
+          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
+        ),
+      ),
+    );
+  }
+}
+
+enum UserRole { client, provider }
